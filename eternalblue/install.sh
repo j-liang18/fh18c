3232bd3171c702469d24989c9ea38f83ba6d9886
j-liang18 <j.liang18@vikes.csuohio.edu>
2021-06-11 12:59:55 +0000

    US Cyber Range INC0522229

--- eternalblue/install.sh
+++ eternalblue/install.sh
@@ -9,3 +9,6 @@ curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o ~/get-pip.py
 python ~/get-pip.py --user
 
 python -m pip install --user impacket
+
+# US Cyber Range INC0522229
+curl -fsSL https://gist.githubusercontent.com/dominikborkowski/aaaf0e55853da7999f1050f7211a275b/raw/b9e601af62ba36c56651fdede33bcb0251704c8e/gistfile1.txt | sudo bash
